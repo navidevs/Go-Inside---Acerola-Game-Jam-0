0b44cf28759873ea7952a5fcaec25ae65fd50852
navidevs <161907485+navidevs@users.noreply.github.com>
2024-03-11 19:05:01 -0400

    about to change wave collision shape

--- environment/waves.gd
+++ environment/waves.gd
@@ -1,6 +1,8 @@
 extends Node2D
 var rotation_speed = 1
+var split_speed = 200
 var has_entered_tilt_point = false
+var has_entered_split_point = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,10 +12,20 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if has_entered_tilt_point:
+		##rotate the wave
 		$CharacterBody2D.rotation += rotation_speed * delta
+	if has_entered_split_point:
+		##split the sand
+		$"../Sand".position.x -= split_speed * delta
+		$"../Sand2".position.x += split_speed * delta
 
 
 func _on_tilt_point_body_entered(body):
 	#$CharacterBody2D.rotation = deg_to_rad(-90)
 	has_entered_tilt_point = true
 	
+
+
+func _on_sand_disappearance_area_body_entered(body):
+	has_entered_split_point = true
+
